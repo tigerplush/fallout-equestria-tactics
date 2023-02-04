1cd35a82bdfffa6928b289995844bad295d902b0
tigerplush <64077786+tigerplush@users.noreply.github.com>
2023-02-04 14:00:02 +0100

    Added zooming

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, input::mouse::MouseWheel};
 use bevy_inspector_egui::{quick::WorldInspectorPlugin, egui::Key};
 use bevy_scene_hook::HookPlugin;
 
@@ -18,6 +18,7 @@ use level_loader_plugin::LevelLoaderPlugin;
 fn main() {
     App::new()
         .insert_resource(LevelName::default())
+        .insert_resource(CurrentZoom(0.0))
         .add_state(ClientState::WaitingToConnect)
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin)
@@ -27,6 +28,7 @@ fn main() {
         .add_plugin(HookPlugin)
         .add_startup_system(setup)
         .add_system(move_camera)
+        .add_system(set_zoom)
         .add_startup_system(spawn_scene)
         .run();
 }
@@ -35,9 +37,10 @@ fn setup(
     mut commands: Commands,
 ) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(5.0, 5.0, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(10.0, 10.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
         projection: Projection::Orthographic(OrthographicProjection {
             scale: 0.01,
+            near: -10.0,
             ..default()
         }),
         ..default()
@@ -45,11 +48,29 @@ fn setup(
 }
 
 const MOVE_SPEED: f32 = 8.0;
+const MIN_ZOOM: f32 = 0.01;
+const MAX_ZOOM: f32 = 0.04;
+const ZOOM_SPEED: f32 = 2.0;
+
+#[derive(Resource)]
+struct CurrentZoom(f32);
+
+impl CurrentZoom {
+    pub fn add(&mut self, zoom: f32) {
+        self.0 = (self.0 + zoom * ZOOM_SPEED).clamp(0.0, 1.0);
+    }
+
+    pub fn get_mapped(&self) -> f32 {
+        MIN_ZOOM + (MAX_ZOOM - MIN_ZOOM) * self.0
+    }
+}
 
 fn move_camera(
     key_input: Res<Input<KeyCode>>,
     mut query: Query<&mut Transform, With<Camera>>,
     time: Res<Time>,
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut current_zoom: ResMut<CurrentZoom>,
 ) {
     let mut dir = Vec3::splat(0.0);
     if key_input.pressed(KeyCode::A) {
@@ -69,8 +90,24 @@ fn move_camera(
         dir.z -= 1.0;
     }
 
+    for event in mouse_wheel_events.iter() {
+        current_zoom.add(-event.y * time.delta_seconds());
+        info!("{}", current_zoom.0);
+    }
+
     for mut transform in &mut query {
         transform.translation += dir.normalize_or_zero() * time.delta_seconds() * MOVE_SPEED;
+    }
+}
+
+fn set_zoom(
+    mut query: Query<&mut Projection>,
+    current_zoom: Res<CurrentZoom>
+) {
+    for mut projection in &mut query {
+        if let Projection::Orthographic(ref mut ortho) = *projection {
+            ortho.scale = current_zoom.get_mapped();
+        }
     }
 }
 
