d27656c3f8595c0340d7a1e50efce3e3d0f81ff2
tigerplush <64077786+tigerplush@users.noreply.github.com>
2023-02-11 22:25:17 +0100

    can now cast rays

--- src/bin/client/level_loader_plugin.rs
+++ src/bin/client/level_loader_plugin.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, asset::LoadState};
+use bevy_rapier3d::prelude::{ComputedColliderShape, Collider, RapierColliderHandle};
 use bevy_renet::renet::{RenetClient, DefaultChannel};
 use bevy_scene_hook::*;
 
@@ -10,11 +11,15 @@ pub struct LevelLoaderPlugin;
 
 impl Plugin for LevelLoaderPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(AssetsLoading(Vec::new()));
         app.add_system_set(
-            SystemSet::on_enter(ClientState::LoadingLevel).with_system(load_level)
+            SystemSet::on_enter(ClientState::LoadingLevel)
+                .with_system(load_level)
+                .with_system(check_load_completed)
         );
         app.add_system_set(
-            SystemSet::on_update(ClientState::LoadingLevel).with_system(update_state)
+            SystemSet::on_update(ClientState::LoadingLevel)
+                .with_system(add_collider)
         );
         app.add_system_set(
             SystemSet::on_exit(ClientState::LoadingLevel).with_system(notify_server)
@@ -23,14 +28,22 @@ impl Plugin for LevelLoaderPlugin {
     }
 }
 
+#[derive(Resource)]
+struct AssetsLoading(Vec<HandleUntyped>);
+
 fn load_level(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     level_name: Res<LevelName>,
+    mut loading: ResMut<AssetsLoading>,
 ) {
+    let level_handle = asset_server.load(level_name.0.clone());
+
+    loading.0.push(level_handle.clone_untyped());
+
     commands.spawn(HookedSceneBundle {
         scene: SceneBundle {
-            scene: asset_server.load(level_name.0.clone()),
+            scene: level_handle,
             ..default()
         },
         hook: SceneHook::new(|entity, cmds| {
@@ -45,16 +58,42 @@ fn load_level(
     info!("Level {} loaded", level_name.0);
 }
 
-fn update_state(
-    query: Query<&Spawnpoint>,
-    mut app_state: ResMut<State<ClientState>>,
-) {
-    for _spawnpoint in &query {
-        info!("spawnpoint detected");
+fn add_collider(
+    query: Query<(Entity, &Handle<Mesh>), Without<RapierColliderHandle>>,
+    meshes: Res<Assets<Mesh>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+)
+{
+    for (entity, mesh) in &query {
+        match asset_server.get_load_state(mesh) {
+            LoadState::Loaded => {
+                let collider = Collider::from_bevy_mesh(
+                    meshes.get(mesh).unwrap(),
+                    &ComputedColliderShape::TriMesh,
+                ).unwrap();
+                commands.entity(entity).insert(collider);
+            }
+            _ => ()
+        }
     }
-    if !query.is_empty() {
-        info!("Exiting loading level state");
-        app_state.set(ClientState::LevelLoaded).unwrap();
+}
+
+fn check_load_completed(
+    query: Query<Entity, (With<Handle<Mesh>>, Without<RapierColliderHandle>)>,
+    asset_server: Res<AssetServer>,
+    loading: Res<AssetsLoading>,
+    mut app_state: ResMut<State<ClientState>>
+)
+{
+    match asset_server.get_group_load_state(loading.0.iter().map(|h| h.id)) {
+        LoadState::Loaded => {
+            if query.is_empty() {
+                info!("everything loaded");
+                app_state.set(ClientState::LevelLoaded).unwrap();
+            }
+        }
+        _ => ()
     }
 }
 
--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,6 +1,8 @@
 use bevy::{prelude::*, input::mouse::MouseWheel};
 use bevy_inspector_egui::{quick::WorldInspectorPlugin, egui::Key};
+use bevy_rapier3d::prelude::{RapierContext, QueryFilter, RapierPhysicsPlugin, NoUserData};
 use bevy_scene_hook::HookPlugin;
+
 
 mod client_plugin;
 use client_plugin::*;
@@ -21,6 +23,7 @@ fn main() {
         .insert_resource(CurrentZoom(0.0))
         .add_state(ClientState::WaitingToConnect)
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(WorldInspectorPlugin)
         .add_plugin(ClientPlugin)
         .add_plugin(LevelLoaderPlugin)
@@ -29,6 +32,7 @@ fn main() {
         .add_startup_system(setup)
         .add_system(move_camera)
         .add_system(set_zoom)
+        .add_system(cast_ray)
         .add_startup_system(spawn_scene)
         .run();
 }
@@ -92,7 +96,6 @@ fn move_camera(
 
     for event in mouse_wheel_events.iter() {
         current_zoom.add(-event.y * time.delta_seconds());
-        info!("{}", current_zoom.0);
     }
 
     for mut transform in &mut query {
@@ -118,4 +121,50 @@ fn spawn_scene(
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
+}
+
+fn cast_ray(
+    mouse_input: Res<Input<MouseButton>>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
+    windows: Res<Windows>,
+    rapier_context: Res<RapierContext>,
+) {
+    if mouse_input.just_pressed(MouseButton::Left) {
+        info!("click");
+        for (camera, camera_transform) in &cameras {
+            let (ray_pos, ray_dir) = ray_from_mouse_position(windows.get_primary().unwrap(), camera, camera_transform);
+            info!("Casting ray from {} along {}", ray_pos, ray_dir);
+            let hit = rapier_context.cast_ray(
+                ray_pos,
+                ray_dir,
+                f32::MAX,
+                true,
+                QueryFilter::new()
+            );
+
+            if let Some((entity, _toi)) = hit {
+                info!("Hit entity {:?}", entity);
+            }
+        }
+    }
+}
+
+fn ray_from_mouse_position(
+    window: &Window,
+    camera: &Camera,
+    camera_transform: &GlobalTransform,
+) -> (Vec3, Vec3) {
+    let mouse_position = window.cursor_position().unwrap_or(Vec2::new(0.0, 0.0));
+
+    let x = 2.0 * (mouse_position.x / window.width() as f32) - 1.0;
+    let y = 2.0 * (mouse_position.y / window.height() as f32) - 1.0;
+
+    let camera_inverse_matrix = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+    let near = camera_inverse_matrix * Vec3::new(x, y, -1.0).extend(1.0);
+    let far = camera_inverse_matrix * Vec3::new(x, y, 1.0).extend(1.0);
+
+    let near = near.truncate() / near.w;
+    let far = far.truncate() / far.w;
+    let dir: Vec3 = far - near;
+    (near, dir)
 }
